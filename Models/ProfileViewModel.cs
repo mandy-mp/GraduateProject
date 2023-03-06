e41e4e7fd14eb006dafc256381f2aa9907053453
Mandy <66321924+mandy-mp@users.noreply.github.com>
2023-03-06 16:18:00 +0200

    added profile page

---
+++ Models/ProfileViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.Metrics;
+using System.Xml.Linq;
+
+namespace GraduateProject.Models
+{
+    public class ProfileViewModel
+    {
+        public string? Name { get; set; }
+
+        public string? Surname { get; set; }
+
+        public string? DOB { get; set; }
+
+        public int Age { get; set; }
+
+        public string? Country { get; set; }
+
+        public string? City { get; set; }
+    }
+}
