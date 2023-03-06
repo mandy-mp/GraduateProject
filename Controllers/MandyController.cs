ba1513d6ef56254b65370bda0b2bfc3a73b8f570
Mandy <66321924+mandy-mp@users.noreply.github.com>
2023-03-06 14:15:48 +0200

    added a new page

---
+++ Controllers/MandyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GraduateProject.Controllers
+{
+    public class MandyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
