de04caed7e7f3211d15f15bf41028096f2367964
Mandy <66321924+mandy-mp@users.noreply.github.com>
2023-03-08 13:59:00 +0200

    fixed date on edit

--- Models/Profile.cs
+++ Models/Profile.cs
@@ -11,8 +11,6 @@ namespace GraduateProject.Models
         [DisplayName("Last Name")]
         public string? Surname { get; set; }
 
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:MMM dd, yyyy}")]
         public DateTime Date_Of_Birth { get; set; }
 
         public int Age { get; set; }
