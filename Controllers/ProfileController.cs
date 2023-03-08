14da017fa6fe54ea6aa85451e00493a608ed75a4
Mandy <66321924+mandy-mp@users.noreply.github.com>
2023-03-08 09:52:48 +0200

    adding CRUD functionality

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -1,18 +1,109 @@
-﻿using GraduateProject.Models;
+﻿using GraduateProject.Data;
+using GraduateProject.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GraduateProject.Controllers
 {
     public class ProfileController : Controller
     {
+        private readonly ApplicationDbContext context;
+
+        public ProfileController (ApplicationDbContext dbContext)
+        {
+            context = dbContext;
+        }
         public IActionResult Index()
         {
-            return View(new ProfileViewModel { Name = "Example", 
-                                               Surname = "One", 
-                                               DOB =  "2000/02/20", 
-                                               Age = 23, 
-                                               Country = "South Africa", 
-                                               City = "Pretoria" });
+
+            return View(context.Profile.ToList());
         }
+
+        //GET: Home/Profile/Create
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        // POST: Home/Create
+        [HttpPost]
+        public IActionResult Create(Profile profile)
+        {
+            try
+            {
+                context.Add(profile);
+                context.SaveChanges();
+                
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return View();
+            }
+        }
+
+        //GET: Home/Profile/Edit/{id}
+        public IActionResult Edit(int Id)
+        {
+            var profile = context.Profile.Find(Id);
+            if (profile != null)
+            {
+                return View(profile);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+               
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Profile profile)
+        {
+            if (profile != null)
+            {
+                try
+                {
+                    context.Update(profile);
+                    context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    return View() ;
+                }
+               
+            }
+            else
+            {
+                return View() ;
+            }
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            var profile = context.Profile.Find(Id);
+            if (profile != null)
+            {
+                try
+                {
+                    context.Profile.Remove(profile);
+                    context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    return View();
+                }
+               
+            }
+            else
+            { 
+                return View(); 
+            }
+           
+        }
+
+
     }
 }
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GraduateProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraduateProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    { 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Profile> Profile { get; set; }
+    }
+
+       
+}
